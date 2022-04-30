ed6ce3c4e6c12839303579e0ce9a8d41672fcc13
121User <vladislavfarukov2004@gmail.com>
2022-04-30 14:52:46 +0700

    сделал2

---
+++ app/src/main/java/com/example/a1545323/zxc2.kt
@@ -0,0 +1,11 @@
+package com.example.a1545323
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class zxc2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zxc2)
+    }
+}
